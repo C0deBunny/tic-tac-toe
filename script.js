6bce9275cfdff7c35c716fea7a9f1be26bd02c1a
Usagi <118476704+C0deBunny@users.noreply.github.com>
2025-05-11 15:16:30 +0200

    Complete board and createplayer. add gameControllr

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const gameBoard = (function () {
-	let board = ["", "", "", "", "", "", "", "", ""]
+	let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 	const getBoard = () => board
 
 	const drawCell = (index, marker) => {
-		if (index >= 0 && index < board.length && board[index] === "") {
+		if (index >= 0 && index < board.length && board[index] === " ") {
 			board[index] = marker
 		}
 	}
 
 	const resetBoard = () => {
-		board = ["", "", "", "", "", "", "", "", ""]
+		board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 	}
 
-	return { getBoard, drawCell, resetBoard }
+	const printBoard = () => {
+		console.log(`[${board[0]}] [${board[1]}] [${board[2]}]\n[${board[3]}] [${board[4]}] [${board[5]}]\n[${board[6]}] [${board[7]}] [${board[8]}]`)
+	}
+
+	return { getBoard, drawCell, resetBoard, printBoard }
 })()
 
 const createPlayer = function (name, marker) {
@@ -23,10 +27,41 @@ const createPlayer = function (name, marker) {
 	}
 }
 
-// testing
-const player1 = createPlayer("Usagi", "O")
-const player2 = createPlayer("Apple Toast", "X")
+const gameController = (function () {
+	const winConditions = [
+		[0, 1, 2], // Row 1
+		[3, 4, 5], // Row 2
+		[6, 7, 8], // Row 3
+		[0, 3, 6], // Col 1
+		[1, 4, 7], // Col 2
+		[2, 5, 8], // Col 3
+		[0, 4, 8], // Diagonal TL-BR
+		[2, 4, 6], // Diagonal TR-BL
+	]
 
-console.log(`name: ${player1.getName()}, marker: ${player1.getMarker()}`)
+	const checkWin = () => {
+		const board = gameBoard.getBoard()
 
-console.log(gameBoard.getBoard())
+		for (let condition of winConditions) {
+			const [a, b, c] = condition
+
+			if (board[a] !== " " && board[a] == board[b] && board[b] == board[c]) {
+				return board[a]
+			}
+		}
+
+		return null
+	}
+
+	const startGame = () => {
+		// starts a new game
+		gameBoard.resetBoard()
+
+		const player1 = createPlayer("Usagi", "O")
+		const player2 = createPlayer("Apple Toast", "X")
+
+		gameBoard.printBoard()
+	}
+
+	return { startGame, checkWin }
+})()
