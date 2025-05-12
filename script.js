72842f60372133242a41cbc2384fc5d18f50e207
Usagi <118476704+C0deBunny@users.noreply.github.com>
2025-05-12 20:39:55 +0200

    Finish startGame() and add a turn swap

--- script.js
+++ script.js
@@ -27,7 +27,17 @@ const createPlayer = function (name, marker) {
 	}
 }
 
+// Temporary players
+const player1 = createPlayer("Usagi", "O")
+const player2 = createPlayer("Apple Toast", "X")
+
 const gameController = (function () {
+	let playerTurn = player1
+
+	const swapTurn = () => {
+		playerTurn === player1 ? (playerTurn = player2) : (playerTurn = player1)
+	}
+
 	const winConditions = [
 		[0, 1, 2], // Row 1
 		[3, 4, 5], // Row 2
@@ -53,14 +63,55 @@ const gameController = (function () {
 		return null
 	}
 
+	const getValidMove = (player) => {
+		let index
+		const board = gameBoard.getBoard()
+
+		while (true) {
+			index = parseInt(prompt(`${player.getName()}'s turn! [${player.getMarker()}] Choose a cell (0-8): `), 10)
+
+			if (!isNaN(index) && index >= 0 && index < 9 && board[index] === " ") {
+				break
+			} else {
+				alert("Invalid move. Choose a different cell")
+			}
+		}
+
+		return index
+	}
+
 	const startGame = () => {
 		// starts a new game
 		gameBoard.resetBoard()
-
-		const player1 = createPlayer("Usagi", "O")
-		const player2 = createPlayer("Apple Toast", "X")
-
+		console.log("Start New Game!")
 		gameBoard.printBoard()
+
+		let currentPlayer = playerTurn
+		const MAX_TURNS = 9
+		let turn = 0
+
+		while (true) {
+			let move = getValidMove(currentPlayer)
+			gameBoard.drawCell(move, currentPlayer.getMarker())
+			gameBoard.printBoard()
+
+			if (checkWin()) {
+				console.log(`${currentPlayer.getName()} Won!`)
+				break
+			}
+
+			turn++
+			if (turn >= MAX_TURNS) {
+				console.log("It's a Tie!")
+				break
+			}
+
+			console.log("-------------------")
+
+			currentPlayer === player1 ? (currentPlayer = player2) : (currentPlayer = player1)
+		}
+
+		swapTurn()
 	}
 
 	return { startGame, checkWin }
